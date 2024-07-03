32ae2f599e997706a65d726e6f4b994e1f8d4c0e
Nikki Vaughan <vnikki13@gmail.com>
2024-07-03 19:12:52 -0400

    Create different RevenueCat webhooks for Android and Apple purchases

--- functions/routes/RCWebhooks.js
+++ functions/routes/RCWebhooks.js
@@ -3,9 +3,17 @@ import {createSubscription} from '../clients/Stripe.js'
 
 const router = new Router()
 
-router.post('/revenueCatWebhook', async (req, res) => {
+router.post('/revenueCatWebhookAndroid', async (req, res) => {
   const userId = req.body.event.app_user_id
-  res.send('Called the RevenueCat Webhook')
+  res.send('Called the RevenueCat Webhook - Android')
+
+  // Save this information to Stripe
+  await createSubscription(userId)
+})
+
+router.post('/revenueCatWebhookApple', async (req, res) => {
+  const userId = req.body.event.app_user_id
+  res.send('Called the RevenueCat Webhook - Apple')
 
   // Save this information to Stripe
   await createSubscription(userId)
